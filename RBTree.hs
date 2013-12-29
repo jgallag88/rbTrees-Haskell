f3f4af0578e631c98864aa1550c4940952f1e6cc
John Gallagher <jgallag88@gmail.com>
2013-12-29 16:50:34 -0500

    Adds Color field for Empty constructor.
    Will be used to add a 'double black' color for use in deletion function.

--- RBTree.hs
+++ RBTree.hs
@@ -2,17 +2,17 @@
 
 module RBTree where
 
-data Color = R | B deriving (Show, Eq)
-data Tree a = Empty
+data Color = R | B | BB deriving (Show, Eq)
+data Tree a = Empty Color
               | Node Color !(Tree a) a !(Tree a) deriving (Show, Eq)
 
 insert :: Ord a => a -> Tree a -> Tree a
 insert x tree = Node B lTree val rTree
         where Node _ lTree val rTree = ins x tree
-              ins x Empty = Node B Empty x Empty
+              ins x (Empty _) = Node B (Empty B) x (Empty B)
               ins x (Node c lTree val rTree)
-                  | lTree == Empty && x <  val = (Node c (Node R Empty x Empty) val rTree)
-                  | rTree == Empty && x >= val = (Node c lTree val (Node R Empty x Empty))
+                  | lTree == Empty B && x <  val = (Node c (Node R (Empty B) x (Empty B)) val rTree)
+                  | rTree == Empty B && x >= val = (Node c lTree val (Node R (Empty B) x (Empty B)))
                   | x >= val                   = balance $ Node c lTree val (ins x rTree)
                   | otherwise                  = balance $ Node c (ins x lTree) val rTree
 
@@ -26,7 +26,7 @@ balance (Node B a x (Node R b y (Node R c z d))) = (Node R (Node B a x b) y (Nod
 balance t = t
 
 member :: Ord a => a -> Tree a -> Bool
-member _ Empty = False
+member _ (Empty _) = False
 member x (Node _ lTree val rTree)
     | x < val = member x lTree
     | x > val = member x rTree
@@ -35,7 +35,7 @@ member x (Node _ lTree val rTree)
 --Pretty printer
 pprint :: Show a => Tree a -> IO () 
 pprint = putStrLn . concat . prnt
-    where prnt Empty = ["----NIL\n"]  --["\n", "----NIL\n", "\n"] Nicer spacing but too much room
+    where prnt (Empty _) = ["----NIL\n"]  --["\n", "----NIL\n", "\n"] Nicer spacing but too much room
           prnt (Node c lTree value rTree) =
                      (preSpace (prnt rTree) ' ' '|') ++ 
                      ["----" ++ "(" ++ show c ++ ") " ++ show value ++ "\n"] ++ 
@@ -52,8 +52,8 @@ checkInvariant t = check t 0
           check (Node R _ _ (Node R _ _ _)) blkH = False
           check (Node R l _ r) blkH = (check l blkH) && (check r blkH)
           check (Node B l _ r) blkH = (check l $ blkH + 1) && (check r $ blkH + 1) 
-          check Empty blkH = blkH == blkHeight
+          check (Empty _) blkH = blkH == blkHeight
           blkHeight = bh t 0
           bh (Node B l _ _) height = bh l $ height + 1
           bh (Node R l _ _) height = bh l height
-          bh Empty          height = height
+          bh (Empty _)      height = height
